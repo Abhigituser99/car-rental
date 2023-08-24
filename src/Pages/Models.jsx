2debd3c85a61d054e38946e87a382e960ccf8d6a
Abhigituser99test <abhishekdav1999@gmail.com>
2023-08-24 18:23:59 +0530

    first comitt

---
+++ src/Pages/Models.jsx
@@ -0,0 +1,302 @@
+import Footer from "../components/Footer";
+import HeroPages from "../components/HeroPages";
+import CarImg1 from "../images/cars-big/aura.jpg";
+import CarImg2 from "../images/cars-big/brezza.webp";
+import CarImg3 from "../images/cars-big/innova.jpg";
+import CarImg4 from "../images/cars-big/nexon.jpg";
+import CarImg5 from "../images/cars-big/swift.jpg";
+import CarImg6 from "../images/cars-big/xuv-300.webp";
+import { Link } from "react-router-dom";
+
+
+function Models() {
+  return (
+    <>
+    
+
+
+
+
+
+       <section className="models-section">
+        <HeroPages name="Vehicle Models" />
+        <div className="container">
+          <div className="models-div">
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg1} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Hyundai Aura</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹2500</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp;Aura
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                     Petrol &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg2} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Maruti Brezza</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹3200</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Brezza
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Auto.
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                     Petrol &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg3} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Toyota Innova</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹5000</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Innova
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg4} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Tata Nexon</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹3500</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Nexon
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg5} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p> XUV-300</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹3700</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; XUV
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg6} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Maruti Swift ZXI</p>
+                      <span>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>₹2500</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Swift
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                    <span>
+                      <i className="fa-solid fa-car-side"></i> &nbsp; Auto.
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Petrol &nbsp; <i className="fa-solid fa-car-side"></i>
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="book-banner">
+          <div className="book-banner__overlay"></div>
+          <div className="container">
+            <div className="text-content">
+              <h2>Book a car by getting in touch with us</h2>
+              <span>
+                <i className="fa-solid fa-phone"></i>
+                <h3>(123) 456-7869</h3>
+              </span>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </section> 
+    </>
+  );
+}
+
+export default Models;
---
+++ src/components/CarData.js
@@ -0,0 +1,93 @@
+import Aura from "../images/cars-big/aura.jpg";
+import Brezza from "../images/cars-big/brezza.webp";
+import Innova from "../images/cars-big/innova.jpg";
+import Nexon from "../images/cars-big/nexon.jpg";
+import Swift from "../images/cars-big/swift.jpg";
+import Xuv from "../images/cars-big/xuv-300.webp";
+
+export const CAR_DATA = [
+  [
+    {
+      name: "Hyundai  Aura",
+      price: "2500",
+      img: Aura,
+      model: "Aura",
+      mark: "Hyundai",
+      year: "2021",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "1197 cc Petrol engine",
+    },
+  ],
+  [
+    {
+      name: "Maruti Brezza",
+      price: "3200",
+      img: Brezza,
+      model: "Brezza",
+      mark: "Maruti",
+      year: "2022",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "1462 cc Petrol ",
+    },
+  ],
+  [
+    {
+      name: "Toyota Innova",
+      price: "5000",
+      img:Innova,
+      model: "Innova",
+      mark: "Toyota",
+      year: "2016",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "2393 cc Hybrid Petrol",
+    },
+  ],
+  [
+    {
+      name: "Tata Nexon",
+      price: "3500",
+      img: Nexon,
+      model: "Nexon",
+      mark: "Tata",
+      year: "2019",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "1.0L Turbo Charged Diesel",
+    },
+  ],
+  [
+    {
+      name: "Mahindra XUV-300",
+      price: "3700",
+      img: Xuv,
+      model: "XUV-300",
+      mark: "Mahindra",
+      year: "2020",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "1.5L Diesel",
+    },
+  ],
+  [
+    {
+      name: "Maruti Swift ZXI",
+      price: "2500",
+      img: Swift,
+      model: "Swift ZXI",
+      mark: "Maruti",
+      year: "2019",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "1.2L Naturally Aspirated Petrol ",
+    },
+  ],
+];
